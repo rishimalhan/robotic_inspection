5c16dfd07a3520d4874b09c901e537e9e6212133
rishimalhan <rmalhan0112@gmail.com>
2021-09-22 17:48:55 -0700

    planning tests for collecting pointcloud date

---
+++ src/planning_tests/src/planning_tests/plan_p2p.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3
+
+import rospy
+import moveit_msgs
+from moveit_commander.robot import RobotCommander
+from moveit_commander.planning_scene_interface import PlanningSceneInterface
+from moveit_commander.move_group import MoveGroupCommander
+
+def main():
+    rospy.init_node("plan_p2p")
+    robot = RobotCommander()
+    scene = PlanningSceneInterface()
+    group_name = "manipulator"
+    move_group = MoveGroupCommander(group_name)
+
+    display_trajectory_publisher = rospy.Publisher(
+        "/move_group/display_planned_path",
+        moveit_msgs.msg.DisplayTrajectory,
+        queue_size=20,
+    )
+
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_group.get_planning_frame()
+    print("============ Planning frame: %s" % planning_frame)
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+    print("============ End effector link: %s" % eef_link)
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print("============ Available Planning Groups:", robot.get_group_names())
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print("============ Printing robot state")
+    print(robot.get_current_state())
+    print("")
+
+
+if __name__=='__main__':
+    main()

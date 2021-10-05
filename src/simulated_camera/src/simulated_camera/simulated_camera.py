9eb63f669f567ec1a0b3e60ee87e9872f9524226
rishimalhan <rmalhan0112@gmail.com>
2021-10-05 11:11:22 -0700

    ...

--- src/simulated_camera/src/simulated_camera/simulated_camera.py
+++ src/simulated_camera/src/simulated_camera/simulated_camera.py
@@ -2,14 +2,15 @@ import open3d
 import numpy
 
 class SimCamera:
-    def __init__(self,stl_path=None):
+    def __init__(self,part_stl=None):
         self.latest_cloud = None
-        self.stl_path = stl_path
-        if stl_path:
-            self.default_cloud = pcd = open3d.io.read_point_cloud(self.stl_path)
+        self.part_stl = part_stl
+        if part_stl:
+            self.default_cloud = open3d.io.read_point_cloud(self.part_stl)
         pass
     def capture_point_cloud(self):
-        self.latest_cloud = self.default_cloud
+        if self.part_stl:
+            self.latest_cloud = self.default_cloud
         pass
     def get_vision_coordinates(self, camera_transform):
         pass

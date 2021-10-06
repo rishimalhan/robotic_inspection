5822a57168197afbcc14cbe60b498dc792d38613
rishimalhan <rmalhan0112@gmail.com>
2021-10-06 12:14:34 -0700

    Visualization bug fixed

--- src/simulated_camera/src/simulated_camera/simulated_camera.py
+++ src/simulated_camera/src/simulated_camera/simulated_camera.py
@@ -16,7 +16,6 @@ class SimCamera:
         self.latest_cloud = None
         self.ros_cloud = None
         self.default_mesh = None
-        self.visible_cloud = open3d.geometry.PointCloud()
         self.part_stl_path = part_stl_path
         self.num_sample_points = num_sample_points
         self.inspection_bot = inspection_bot
@@ -45,20 +44,21 @@ class SimCamera:
             self.capture_point_cloud()
             self.ros_cloud = convertCloudFromOpen3dToRos(cloud, frame_id="base")
             stl_cloud_pub.publish(self.ros_cloud)
-            if not self.visible_cloud.is_empty():
-                self.visible_ros_cloud = convertCloudFromOpen3dToRos(self.visible_cloud, frame_id="base")
-                visible_cloud_pub.publish(self.visible_ros_cloud)
+            self.visible_ros_cloud = convertCloudFromOpen3dToRos(self.visible_cloud, frame_id="base")
+            visible_cloud_pub.publish(self.visible_ros_cloud)
             rospy.sleep(0.2)
         
     def capture_point_cloud(self, fit_kdtree=False):
+        self.visible_cloud = open3d.geometry.PointCloud()
         if self.part_stl_path:
             fk = self.inspection_bot.get_forward_kinematics()
             # Find the neighbors on the part within camera view
             [k, idx, _] = self.stl_kdtree.search_hybrid_vector_3d( fk[0:3,3],
-                                        max_nn=int(self.num_sample_points/20), radius=0.05 )
-            stl_points = numpy.asarray(self.default_cloud.points)
-            visible_points = stl_points[idx,:]
-            self.visible_cloud.points = open3d.utility.Vector3dVector( visible_points )
+                                        max_nn=int(self.num_sample_points/20), radius=0.3 )
+            if idx:
+                stl_points = numpy.asarray(self.default_cloud.points)
+                visible_points = stl_points[idx,:]
+                self.visible_cloud.points = open3d.utility.Vector3dVector( visible_points )
             
 
     def get_vision_coordinates(self, camera_transform):
